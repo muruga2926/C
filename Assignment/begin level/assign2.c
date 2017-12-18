41589c2f88bba94c4d9e0978c5ecc3fa981a7f25
muruga2926 <34641761+muruga2926@users.noreply.github.com>
2017-12-18 14:39:15 +0530

    Create assign2.c

---
+++ Assignment/begin level/assign2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+	char a[30]="Sachin Ramesh Tendulkar";
+	char b[20]="Little Master";
+	int c=18426;
+	double d=44.83;
+	int e=463;
+	printf("%s",a);
+	printf("\n%s",b);
+	printf("\n%d",c);
+	printf("\n%0.2f",d);
+	printf("\n%d",e);
+	return 0;
+}
